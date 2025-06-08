03d600c1cfd452cb9b4337851de2c49745711ab7
Raúl S. <98350729+raulscarvalho@users.noreply.github.com>
2025-06-07 21:22:07 -0300

    commit de modificacoes na header, footer e feed (inicio)

--- siteavellozfortaleza/app.js
+++ siteavellozfortaleza/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var modelosRouter = require('./routes/modelos');
 var localizacaoRouter = require('./routes/localizacao');
-//var contatoRouter = require('./routes/contato');
+var contatoRouter = require('./routes/contato');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/modelos', modelosRouter);
 app.use('/localizacao', localizacaoRouter);
-//app.use('/contato', contatoRouter);
+app.use('/contato', contatoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
